8698e51960f35c522aed9807118a020bd62f0dae
AbdiwakBekele <abdiwakbek3226@gmail.com>
2024-07-26 19:57:03 +0300

    updated

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/get_user', [UserController::class, 'index']);
