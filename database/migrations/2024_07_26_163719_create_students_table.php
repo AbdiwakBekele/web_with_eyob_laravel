1de930d28511cd2798f21bdbe7b628834ce662ed
AbdiwakBekele <abdiwakbek3226@gmail.com>
2024-07-29 19:45:34 +0300

    updated

--- database/migrations/2024_07_26_163719_create_students_table.php
+++ database/migrations/2024_07_26_163719_create_students_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->text('name');
+            $table->text('age');
+            $table->text('phone');
+            $table->text('city');
+            $table->text('subcity');
             $table->timestamps();
         });
     }
@@ -20,8 +23,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('students');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,18 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/second', function () {
-    return view('second');
-});
+// Route::get('/home/user', function () {
+//     return view('home');
+// });
 
-Route::get('/get_user', [UserController::class, 'index']);
+// Route::get('/second', function () {
+//     return view('second');
+// });
+
+// Route::get('/get_user', [UserController::class, 'index']);
+
+Route::resource('student', StudentController::class);
