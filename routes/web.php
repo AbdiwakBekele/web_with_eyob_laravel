e1977451528ea455ca98be546331f36b751e8de4
Merdasa-Belay <merdasabelay@gmail.com>
2024-08-02 17:46:57 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,18 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/home/user', function () {
-//     return view('home');
-// });
+Route::get('/home', function () {
+    return view('home');
+});
 
-// Route::get('/second', function () {
-//     return view('second');
-// });
+Route::get('/second', function () {
+    return view('second');
+});
 
-// Route::get('/get_user', [UserController::class, 'index']);
+Route::get('/get_user', [UserController::class, 'index']);
 
 Route::resource('student', StudentController::class);
